0375b9b4901912b40eed0393ed5df6ff2a83bee0
ismailm <ismailm@oclc.org>
2019-08-08 16:17:05 +0200

    add nonce-tracker

--- test/oracles.js
+++ test/oracles.js
@@ -1,23 +1,21 @@
-
 var Test = require('../config/testConfig.js');
 //var BigNumber = require('bignumber.js');
 
 contract('Oracles', async (accounts) => {
 
   const TEST_ORACLES_COUNT = 20;
-
-  
-   // Watch contract events
-   const STATUS_CODE_UNKNOWN = 0;
-   const STATUS_CODE_ON_TIME = 10;
-   const STATUS_CODE_LATE_AIRLINE = 20;
-   const STATUS_CODE_LATE_WEATHER = 30;
-   const STATUS_CODE_LATE_TECHNICAL = 40;
-   const STATUS_CODE_LATE_OTHER = 50;
-
+  const STATUS_CODE_UNKNOWN = 0;
+  const STATUS_CODE_ON_TIME = 10;
+  const STATUS_CODE_LATE_AIRLINE = 20;
+  const STATUS_CODE_LATE_WEATHER = 30;
+  const STATUS_CODE_LATE_TECHNICAL = 40;
+  const STATUS_CODE_LATE_OTHER = 50;
   var config;
   before('setup contract', async () => {
     config = await Test.Config(accounts);
+
+    // Watch contract events
+
 
   });
 
@@ -25,21 +23,15 @@ contract('Oracles', async (accounts) => {
   it('can register oracles', async () => {
     
     // ARRANGE
-    let fee = await config.flightSuretyApp.REGISTRATION_FEE.call()
+    let fee = await config.flightSuretyApp.REGISTRATION_FEE.call();
 
     // ACT
-    for (let a = 1; a < TEST_ORACLES_COUNT; a++) {
-      await config.flightSuretyApp.registerOracle({
-        from: accounts[a],
-        value: fee
-      })
-      let result = await config.flightSuretyApp.getMyIndexes.call({
-        from: accounts[a]
-      })
-      console.log(`Oracle Registered: ${result[0]}, ${result[1]}, ${result[2]}`)
+    for(let a=1; a<TEST_ORACLES_COUNT; a++) {      
+      await config.flightSuretyApp.registerOracle({ from: accounts[a], value: fee });
+      let result = await config.flightSuretyApp.getMyIndexes.call({from: accounts[a]});
+      console.log(`Oracle Registered: ${result[0]}, ${result[1]}, ${result[2]}`);
     }
-  })
-
+  });
 
   it('can request flight status', async () => {
     
@@ -55,33 +47,28 @@ contract('Oracles', async (accounts) => {
     // loop through all the accounts and for each account, all its Indexes (indices?)
     // and submit a response. The contract will reject a submission if it was
     // not requested so while sub-optimal, it's a good test of that feature
-    let errorCount = 0
-    for (let a = 1; a < TEST_ORACLES_COUNT; a++) {
+    for(let a=1; a<TEST_ORACLES_COUNT; a++) {
+
       // Get oracle information
-      let oracleIndexes = await config.flightSuretyApp.getMyIndexes.call({
-        from: accounts[a]
-      })
-      for (let idx = 0; idx < 3; idx++) {
+      let oracleIndexes = await config.flightSuretyApp.getMyIndexes.call({ from: accounts[a]});
+      for(let idx=0;idx<3;idx++) {
+
         try {
           // Submit a response...it will only be accepted if there is an Index match
-          let response = await config.flightSuretyApp.submitOracleResponse(
-            oracleIndexes[idx],
-            config.firstAirline,
-            flight,
-            timestamp,
-            STATUS_CODE_ON_TIME,
-            { from: accounts[a] }
-          )
-        } catch (e) {
-          console.log(
-            '\nError',
-            idx,
-            oracleIndexes[idx].toNumber(),
-            flight,
-            timestamp
-          )
+          await config.flightSuretyApp.submitOracleResponse(oracleIndexes[idx], config.firstAirline, flight, timestamp, STATUS_CODE_ON_TIME, { from: accounts[a] });
+          console.log("found");
         }
+        catch(e) {
+          // Enable this when debugging
+           console.log('\nError', idx, oracleIndexes[idx].toNumber(), flight, timestamp);
+        }
+
       }
     }
-  })
-})
+
+
+  });
+
+
+ 
+});
--- truffle.js
+++ truffle.js
@@ -1,16 +1,28 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+var mnemonic = "mammal apology muffin decide candy around head priority cancel bridge attitude pink";
+var NonceTrackerSubprovider = require("web3-provider-engine/subproviders/nonce-tracker");
 
 module.exports = {
   networks: {
     development: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 50);
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: function () {
+        var wallet = new HDWalletProvider(mnemonic, ENDPOINT)
+        var nonceTracker = new NonceTrackerSubprovider()
+        wallet.engine._providers.unshift(nonceTracker)
+        nonceTracker.setEngine(wallet.engine)
+        return wallet
       },
-      network_id: '*',
-      gas: 9999999
+      network_id: 4,
+      // gas: 2000000,   // <--- Twice as much
+      // gasPrice: 10000000000,
     }
   },
+
   compilers: {
     solc: {
       version: "^0.4.24"
